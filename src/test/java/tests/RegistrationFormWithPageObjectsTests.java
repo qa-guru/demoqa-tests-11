b77869505887af72adf2436cefc222acb5544580
svasenkov <aanher@gmail.com>
2022-02-10 20:50:33 +0300

    fixed, removed 1 registration test

--- src/test/java/tests/RegistrationFormWithPageObjectsTests.java
+++ src/test/java/tests/RegistrationFormWithPageObjectsTests.java
@@ -43,35 +43,6 @@ public class RegistrationFormWithPageObjectsTests {
         $("#submit").click();
 
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").$(byText("Student Name"))
-                .parent().shouldHave(text("Alex Egorov"));
-    }
-
-    @Test
-    void success1FillTest() {
-        open("/automation-practice-form");
-        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
-
-        new RegistrationPage().setFirstName("Alex");
-        new RegistrationPage().setLastName("Egorov");
-        $("#userEmail").setValue("alex@egorov.com");
-        $("#genderWrapper").$(byText("Other")).click();
-        $("#userNumber").setValue("1231231230");
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("July");
-        $(".react-datepicker__year-select").selectOption("2008");
-        $("[aria-label$='July 30th, 2008']").click();
-        $("#subjectsInput").setValue("Math").pressEnter();
-        $("#hobbiesWrapper").$(byText("Sports")).click();
-        $("#uploadPicture").uploadFromClasspath("img/1.png");
-        $("#currentAddress").setValue("Some address 1");
-        $("#state").scrollTo().click();
-        $("#stateCity-wrapper").$(byText("NCR")).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText("Noida")).click();
-        $("#submit").click();
-
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
         new RegistrationPage().checkForm("Student Name", "Alex Egorov");
         new RegistrationPage().checkForm("Student Email", "alex@egorov.com");
         new RegistrationPage().checkForm("Gender", "Other");
