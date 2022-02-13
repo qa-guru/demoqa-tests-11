275d607c3e5a11d7cd9e5590d8ef2918d3c682bc
svasenkov <aanher@gmail.com>
2022-02-13 18:49:28 +0300

    added homework2

---
+++ src/test/java/tests/RegistrationFormWithCommentsTests.java
@@ -0,0 +1,59 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class RegistrationFormWithCommentsTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void successFillTest() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        $("#firstName").setValue("Alex");
+        $("#lastName").setValue("Egorov");
+        $("#userEmail").setValue("alex@egorov.com");
+        $("#gender-radio-1").parent().click();
+//        $("#gender-radio-1").click(); // wrong
+//        $("[for=\"gender-radio-1\"]").click();
+        $("#userNumber").setValue("1231231230");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("2008");
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+//<div class="react-datepicker__day react-datepicker__day--030 react-datepicker__day--outside-month">30</div>
+//<div class="react-datepicker__day react-datepicker__day--030">30</div>
+//        $("[aria-label$='July 30th, 2008']").click(); Bad - th/rd/...
+        $("#subjectsInput").setValue("Maths").pressEnter();
+        $("#hobbies-checkbox-1").parent().click();
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+//        $("#uploadPicture").uploadFile(new File("./src/test/resources/img/1.png"));
+//        File imgFile = new File("./src/test/resources/img/1.png");
+//        $("#uploadPicture").uploadFile(imgFile);
+        $("#currentAddress").setValue("Some street 1");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Alex Egorov"), text("alex@egorov.com"));
+//        $(".table-responsive").$(byText("Student Name"))
+//                .parent().shouldHave(text("Alex Egorov"));
+    }
+}
