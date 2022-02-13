dfd5ec7169521da58cd10d731ae349cc3a4faf81
svasenkov <aanher@gmail.com>
2022-02-13 19:44:35 +0300

    added Faker tests

---
+++ src/test/java/tests/RegistrationFormWithFakerTests.java
@@ -0,0 +1,53 @@
+package tests;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static utils.RandomUtils.getRandomString;
+
+public class RegistrationFormWithFakerTests extends TestBase {
+
+//    Faker faker = new Faker();
+    Faker faker = new Faker(new Locale("ru"));
+    String firstName = faker.name().firstName();
+    String email = faker.internet().emailAddress();
+    String lastName = faker.name().lastName();
+    String currentAddress = faker.rickAndMorty().quote();
+
+    @Test
+    void successFillTest() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("#genterWrapper").$(byText("Other")).click();
+        $("#userNumber").setValue("1231231230");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("2008");
+        $("[aria-label$='July 30th, 2008']").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#currentAddress").setValue(currentAddress);
+        $("#state").scrollTo().click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Noida")).click();
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").$(byText("Student Name"))
+                .parent().shouldHave(text(firstName + " " + lastName));
+        $(".table-responsive").$(byText("Current Address"))
+                .parent().shouldHave(text(currentAddress));
+    }
+}
